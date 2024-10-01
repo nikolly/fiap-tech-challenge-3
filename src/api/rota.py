b0400351a5dee30bb4537ef7c3f20304ca6fdb49
Nikolly Cardoso de Faria <nikolly@syncode.com.br>
2024-09-30 22:57:45 -0300

    Finalizando as APIs de treino do modelo de machine learning

--- src/api/rota.py
+++ src/api/rota.py
@@ -1,34 +1,57 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, request, Blueprint, jsonify
 from src.core.train_model import train_machine_learning_model
-from src.core.predict import predict
+import pickle
+import os
+import pandas as pd
 
+
+colunas = ['temp_max', 'temp_afternoon']
+modelo_path = 'modelo/modelo_regressao_linear.pkl'
+modelo = pickle.load(open(modelo_path,'rb')) if os.path.exists(modelo_path) else None
 
 app = Flask(__name__)
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-# POST /api/data
+# POST /api/train
 @api_bp.route('/train', methods=['POST'])
 def create_data():
     """
     RESTful endpoint to train the machine learning model.
     """
+    global modelo
     try:
-        train_machine_learning_model()
+        response = train_machine_learning_model()
+        if response[1] == 200: 
+            modelo = pickle.load(open('modelo/modelo_regressao_linear.pkl','rb'))
+        return response
     except Exception as e:
         return jsonify({'error': str(e)}), 500  # 500 Internal Server Error
 
 
 # POST /api/predictions
-@api_bp.route('/predictions', methods=['POST'])
+@api_bp.route('/prediction', methods=['POST'])
 def create_prediction():
     """
     RESTful endpoint to generate a prediction from a model.
     """
+    
+    if not modelo:
+        return jsonify({'error': 'Model not trained'}), 500
+    
     try:
-        prediction = predict()
-        return jsonify({'prediction': prediction}), 200  # 200 OK
+        dados = request.get_json()
+        if not dados:
+            return jsonify({'error': 'No data provided'}), 400
+        
+        # Transforme os dados em um DataFrame
+        dados_input = pd.DataFrame([dados], columns=colunas)
+        
+        # Faça a previsão
+        humidity = modelo.predict(dados_input)
+        return jsonify({'humidity': round(humidity[0], 1)})
     except Exception as e:
+        print(e)
         return jsonify({'error': str(e)}), 500  # 500 Internal Server Error
     
 
--- src/core/train_model.py
+++ src/core/train_model.py
@@ -3,8 +3,9 @@ import os
 import json
 from sklearn.model_selection import train_test_split
 from statsmodels.formula.api import ols
-from sklearn.metrics import r2_score
 from src.function.functions import download_data_from_s3
+import pickle
+from flask import jsonify
 
 
 def train_machine_learning_model():
@@ -13,7 +14,7 @@ def train_machine_learning_model():
         s3_folder = 'Silver'
         local_folder = 'data'
         
-        #download_data_from_s3(bucket_name, s3_folder, local_folder)
+        # download_data_from_s3(bucket_name, s3_folder, local_folder)
         dados = get_data_from_files(local_folder)
         df = pd.DataFrame(dados)
 
@@ -25,14 +26,26 @@ def train_machine_learning_model():
         df_train = pd.DataFrame(data= x_train)
         df_train['humidity_afternoon'] = y_train
 
-        modelo_0 = ols('humidity_afternoon ~ temp_max + temp_afternoon', data= df_train).fit()
-        print(modelo_0.rsquared)
-        predict_0 = modelo_0.predict(x_test[['temp_max', 'temp_afternoon']])
-        print("R²: ",r2_score(y_test, predict_0))
+        modelo = ols('humidity_afternoon ~ temp_max + temp_afternoon', data= df_train).fit()
+                
+        caminho_arquivo = 'modelo/modelo_regressao_linear.pkl'
+        diretorio = os.path.dirname(caminho_arquivo)
+
+        # Verificar se o diretório existe, se não, criar
+        if not os.path.exists(diretorio):
+            os.makedirs(diretorio)
+
+        # Salvar o modelo em um arquivo usando pickle
+        with open(caminho_arquivo, 'wb') as arquivo:
+            pickle.dump(modelo, arquivo)
+        
+        data = {'message': 'Model trained successfully'}
+        return jsonify(data), 200
         
     except Exception as e:
         print(e)
-        return f"An unexpected error occurred: {str(e)}"
+        data = {'error': str(e)}
+        return jsonify(data), 500
     
 
 def get_data_from_files(local_folder):
